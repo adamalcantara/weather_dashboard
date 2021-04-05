e4dca198fcd82da86663cace23ef057800ec29ae
Adam Alcantara <adamjalcantara@gmail.com>
2021-04-04 22:34:47 -0400

    Sunday evening commit

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-var apiKey = "371201dc0173d4e3e8307cc1abf9c509";
+var apiKey = "a8413cba25605a4ae619a572d1557fb5";
 var searchBtn = document.querySelector("#search-button");
+var todayEl = document.getElementById("today");
 
 
-
-function searchValue () {
+function searchValue() {
     var searchValue = document.querySelector("#search-value").value;
     console.log(searchValue);
     getWeather(searchValue);
     fiveDayForecast(searchValue);
 }
 
-function getWeather (searchValue) {
+
+
+function getWeather(searchValue) {
     var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + apiKey + "&units=imperial";
     //set up validation if form is blank
     fetch(apiURL)
@@ -20,11 +22,68 @@ function getWeather (searchValue) {
             console.log(data);
             //append content to the DOM here OR with a function
             //call five day forecast
+
+            var cityName = data.name;
+            console.log("city name");
+            console.log(cityName);
+
+            var windSpeed = data.wind.speed;
+
+            var temperature = data.main.temp;
+            console.log("temperature");
+            console.log(temperature);
+
+            var humidity = data.main.humidity;
+            console.log("humidity");
+            console.log(humidity);
+
+            var dateEl = moment().format("MM/DD/YYYY");
+
+            var cityNameEl = document.createElement("h1");
+            $(cityNameEl).text(cityName + " " + dateEl);
+            todayEl.append(cityNameEl);
+
+            var temperatureEl = document.createElement("p");
+            $(temperatureEl).text("Temp: " + temperature + "°F")
+            todayEl.append(temperatureEl);
+
+            var windEl = document.createElement("p");
+            $(windEl).text("Wind: " + windSpeed + " MPH");
+            todayEl.append(windEl);
+
+            var humidityEl = document.createElement("p");
+            $(humidityEl).text("Humidity: " + humidity + " %");
+            todayEl.append(humidityEl);
+
+            var longitude = data.coord.lon;
+            console.log(longitude);
+
+            var latitude = data.coord.lat;
+            console.log(latitude);
+
+            function uvIndex(searchValue) {
+                var uvURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
+                fetch(uvURL)
+                    .then(function (response) {
+                        return response.json()
+                    }).then(function (data) {
+                        console.log(data);
+                    })
+            }
+
+
         })
-        
+
+
+
+
 }
 
-function fiveDayForecast (searchValue) {
+
+
+
+
+function fiveDayForecast(searchValue) {
     var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=" + apiKey + "&units=imperial";
     fetch(fiveDayURL)
         .then(function (response) {
@@ -32,7 +91,8 @@ function fiveDayForecast (searchValue) {
         }).then(function (data) {
             console.log(data);
         })
-    
+
 }
+
 
 searchBtn.addEventListener("click", searchValue);
